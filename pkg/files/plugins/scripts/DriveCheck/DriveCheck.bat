b9879ecaba481fc4e91909df91504e0629ad771a
Robert Vandervoort <bigrobintx@gmail.com>
2017-04-21 16:48:17 -0500

    repack
    whoops fixed folder structure for grid based deploy

---
+++ pkg/files/plugins/scripts/DriveCheck/DriveCheck.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -file %MIBDIRS%\..\plugins\scripts\DriveCheck\DriveCheck.PS1
--- pkg/plugins/scripts/DriveCheck/DriveCheck.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -file C:\uptime\plugins\scripts\DriveCheck\DriveCheck.PS1
